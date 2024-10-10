7f46c61eae96aeb7943c04aff226304d696f1e3a
Silly! <128849436+sillydean@users.noreply.github.com>
2024-10-10 19:32:59 +0200

    hello

---
+++ module2/index.php
@@ -0,0 +1,20 @@
+<?php
+$school = 'Digital School';
+//echo "I love $school";
+
+//int, float,string, boolean, array, object, resources, null
+
+$x = 120;
+$y = 50;
+
+echo $x + $y;
+echo $x - $y;
+echo $x * $y;
+echo $x / $y;
+
+$a = 'Digital';
+$b = 'School';
+$c = $a.$b;
+echo "$c \n";
+?>
+
