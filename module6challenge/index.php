a3bb51e1a9630dc35f798f3af9c42be9058d28d6
Silly! <128849436+sillydean@users.noreply.github.com>
2024-11-05 19:04:07 +0100

    easy challenge

---
+++ module6challenge/index.php
@@ -0,0 +1,21 @@
+<?php
+//EASY CHALLENGE
+function readFromFile(){
+    $file = fopen("easychallenge.txt","r");
+
+    //check if it was opened
+    if($file){
+        echo "Content read of example.txt: ";
+
+        //read file until the end (eof)
+        while(!feof($file)){
+            $line = fgets($file);
+            echo ($line)."<br>";
+        }
+        fclose($file);
+    }else{
+        echo"Failed to open file";
+    }
+}
+readFromFile();
+?>
