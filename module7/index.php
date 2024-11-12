7b2b9a8f8e9097784ecd6cce7bdc066a264229b6
Silly! <128849436+sillydean@users.noreply.github.com>
2024-11-12 18:35:51 +0100

    module 7

--- module7/index.php
+++ module7/index.php
@@ -1,22 +1,38 @@
 <?php
 
 
-//setting mysql database parameters
+// //setting mysql database parameters
+// $host = 'localhost';
+// $user = 'root';
+// $pass = '';
+
+// //connection in databes using pdo
+// //beginning of a try block
+// try{
+//     //attempt to create a new pdo object and connecting to a mysql database
+//     //the connection string is constructed using variables $host, $user, $pass
+
+//     $conn = new PDO("mysql:host=$host", $user, $pass);
+
+//     //if the connection is successful, print connected
+//     echo "Connected";
+// } catch (Exception $e){
+//     echo "Not Connected";
+// }
+
 $host = 'localhost';
 $user = 'root';
 $pass = '';
 
-//connection in databes using pdo
-//beginning of a try block
 try{
-    //attempt to create a new pdo object and connecting to a mysql database
-    //the connection string is constructed using variables $host, $user, $pass
+    $conn = new PDO("mysql:host=$host", $user, $pass);
 
-    $conn = new PDO("mysql:host=$host; $user, $pass");
+    $sql = "CREATE DATABASE testdb";
 
-    //if the connection is successful, print connected
-    echo "Connected";
-} catch (Exception $e){
-    echo "Not Connected";
+    $conn -> exec($sql);
+
+    echo "Database created";
+}catch (Exception $e){
+    echo "Database not created";
 }
 ?>
